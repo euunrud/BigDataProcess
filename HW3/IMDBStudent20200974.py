67513216ddce0fc1ff28eca500e9e9bb6b6771c9
Eunkyy <88434735+Eunkyy@users.noreply.github.com>
2022-11-01 02:52:38 +0900

    Update IMDBStudent20200974.py

--- HW3/IMDBStudent20200974.py
+++ HW3/IMDBStudent20200974.py
@@ -1,18 +1,18 @@
 import sys
 
-gr1=["Comedy", "Fantasy", "Romance", "Animation", "Children's", "Adventure", "Drama", "Action", "Crime", "Thriller","Horror"]
-gr2=[0,0,0,0,0,0,0,0,0,0,0]
-
+genres = dict()
 with open(sys.argv[1], "rt") as fp:
 	for line in fp:
 		str1 = line.split("::")
 		grlist = str1[2].split("|")
 		for g in grlist:
-#			print(g)
-			for i in range(11):
-				if gr1[i] in g:
-					gr2[i] += 1
-					break	
+			g = g.replace("\n", "")
+			if g not in genres:
+				genres[g] = 1
+			else:
+				genres[g] += 1
+
+keys = genres.keys()
 with open(sys.argv[2], "wt") as fp:
-	for i in range(11):
-		fp.write(gr1[i] + " " + str(gr2[i]) + "\n")
+	for key in keys:
+		fp.write(key + " " + str(genres[key]) + "\n")
