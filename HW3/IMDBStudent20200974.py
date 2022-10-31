d45d64df0a2dcc3f371728d8c002c61fdad2d911
Eunkyy <88434735+Eunkyy@users.noreply.github.com>
2022-11-01 02:25:46 +0900

    Update IMDBStudent20200974.py

--- HW3/IMDBStudent20200974.py
+++ HW3/IMDBStudent20200974.py
@@ -1,1 +1,18 @@
-print("hi")
+import sys
+
+gr1=["Comedy", "Fantasy", "Romance", "Animation", "Children's", "Adventure", "Drama", "Action", "Crime", "Thriller","Horror"]
+gr2=[0,0,0,0,0,0,0,0,0,0,0]
+
+with open(sys.argv[1], "rt") as fp:
+	for line in fp:
+		str1 = line.split("::")
+		grlist = str1[2].split("|")
+		for g in grlist:
+#			print(g)
+			for i in range(11):
+				if gr1[i] in g:
+					gr2[i] += 1
+					break	
+with open(sys.argv[2], "wt") as fp:
+	for i in range(11):
+		fp.write(gr1[i] + " " + str(gr2[i]) + "\n")
